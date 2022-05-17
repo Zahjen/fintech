0d27d8d7eda37348572d85de4741ea3232bd6bec
Zahjen <zahjen.dev@gmail.com>
2022-05-17 13:47:34 +0200

    Add files via upload

---
+++ Fintech/controller/question-controller.php
@@ -0,0 +1,191 @@
+<?php
+
+class QuestionController {
+
+    // --------------------
+    // Déclaration des attributs
+    // --------------------
+
+    private $db;
+    private $request_method;
+    private $id_question;
+    private $question_manager;
+
+    // --------------------
+    // Constructeur
+    // --------------------
+
+    public function __construct($db, $request_method, $id_question) {
+        $this->db = $db;
+        $this->request_method = $request_method;
+        $this->id_question = $id_question;
+        $this->question_manager = new QuestionManager($db);
+    }
+
+    // --------------------
+    // Méthode
+    // --------------------
+
+    // Méthode permettant d'exécuter une méthode selon le type de requête envoyée
+    public function execute_query() {
+
+        switch ($this->request_method) {
+            case 'GET':
+                if ($this->id_question) {
+                    $response = $this->get_by_id($this->id_question);
+                } else {
+                    $response = $this->get_all();
+                };
+                break;
+
+            case 'POST':
+                $response = $this->insert();
+                break;
+
+            case 'PUT':
+                $response = $this->update($this->id_question);
+                break;
+
+            case 'DELETE':
+                $response = $this->delete($this->id_question);
+                break;
+
+            default:
+                $response = $this->not_found_query();
+                break;
+        }
+
+        header($response['status_code_header']);
+
+        if ($response['body']) {
+            echo $response['body'];
+        }
+
+    }
+
+    // Méthode permettant de récupérer toutes les questions 
+    private function get_all() {
+
+        $result = $this->question_manager->getAll();
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+
+        header('Content-type: application/json');
+
+        $response['body'] = json_encode($result);
+
+        return $response;
+
+    }
+
+    // Méthode permettant de récupérer une question selon son id
+    private function get_by_id($id) {
+
+        $result = $this->question_manager->getById($id);
+
+        if (!$result) {
+            return $this->not_found_query();
+        }
+
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+
+        return $response;
+
+    }
+
+    // Méthode permettant d'insérer une question dans la base de données
+    private function insert() {
+
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+
+        if (!$this->is_question_valid($input)) {
+            return $this->not_executable_query();
+        }
+
+        $this->question_manager->insert($input);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = null;
+
+        return $response;
+
+    }
+
+    // Méthode permettant de mettre à jour une question selon son id
+    private function update($id) {
+
+        $result = $this->question_manager->getById($id);
+
+        if (! $result) {
+            return $this->not_found_query();
+        }
+
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+
+        if (!$this->is_question_valid($input)) {
+            return $this->not_executable_query();
+        }
+
+        $this->question_manager->update($input);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+
+        return $response;
+
+    }
+
+    // Méthode permettant de supprimer une question selon son id
+    private function delete($id) {
+
+        $result = $this->question_manager->getById($id);
+
+        if (!$result) {
+            return $this->not_found_query();
+        }
+
+        $this->question_manager->delete($result);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+
+        return $response;
+
+    }
+
+    // Méthode permettant de vérifier qu'une question est correcte avant de pouvoir l'insérer ou la mettre à jour dans la base de données
+    private function is_question_valid($input) {
+
+        if (!isset($input['labelQuestion'])) {
+            return false;
+        }
+
+        /*
+        if (!isset($input['type'])) {
+            return false;
+        }*/
+
+        return true;
+
+    }
+
+    // Méthode permettant de notifier qu'une requête n'est pas traitable
+    private function not_executable_query() {
+
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid input'
+        ]);
+
+        return $response;
+
+    }
+
+    // Méthode permettant de notifier que la requête n'a pa donnée suite
+    private function not_found_query() {
+
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+
+        return $response;
+    }
+}
+
+?>
