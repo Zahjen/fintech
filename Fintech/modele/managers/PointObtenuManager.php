30e9ef86c00384e062c3753bb6d515b6c933a111
Bernard NGUYEN <92371585+Sanngithub@users.noreply.github.com>
2022-05-20 08:38:24 +0200

    add function getById(idQuestion, idReponse)
    add function getById(idQuestion, idReponse)

--- Fintech/modele/managers/PointObtenuManager.php
+++ Fintech/modele/managers/PointObtenuManager.php
@@ -30,6 +30,24 @@
             }
             return $monPointObtenu;
         }
+        
+        public function getByIdQuestionIdReponse($idQuestion, $idReponse) {
+            $monPointObtenu = [];
+            try {
+                $req = $this->bdd->prepare('SELECT idQuestion, idReponse, pointQuestion FROM pointobtenu WHERE idQuestion = :idQuestion AND idReponse = :idReponse');
+                $req->bindValue(':idQuestion', $idQuestion, PDO::PARAM_INT);
+                $req->bindValue(':idReponse', $idReponse, PDO::PARAM_INT);
+                $req->execute();
+                while ($donnees = $req->fetch(PDO::FETCH_ASSOC)) {
+                    $pointObt = new PointObtenu();
+                    $pointObt->hydrate($donnees);
+                    $monPointObtenu[] = $pointObt;
+                }
+            } catch (Exception $erreur) {
+                die('Erreur : '.$erreur->getMessage());
+            }
+            return $monPointObtenu;
+        }
 
         public function getAll() {
             $listePointsObtenus = [];
@@ -84,12 +102,25 @@
             }
         }
 
+        // méthode update avec une seule condition dans le WHERE idQuestion = :id 
         public function update(PointObtenu $pointObt) {
             try {
                 $req = $this->bdd->prepare( 'UPDATE pointobtenu SET idReponse = :idReponse, pointQuestion = :pointQuestion WHERE idQuestion = :id');
                 $req->bindValue(':id', $pointObt->getIdQuestion(), PDO::PARAM_INT);
                 $req->bindValue(':idReponse', $pointObt->getIdReponse(), PDO::PARAM_INT);
                 $req->bindValue(':pointQuestion', $pointObt->getPointQuestion());
+                $req->execute();
+            } catch (Exception $erreur) {
+                die('Erreur : '.$erreur->getMessage());
+            }
+        }
+
+        // méthode update avec deux conditions dans le WHERE idQuestion = :id et idReponse = :idReponse
+        public function update2($idQuestion, $idReponse) {
+            try {
+                $req = $this->bdd->prepare( 'UPDATE pointobtenu SET idReponse = :idReponse WHERE idQuestion = :id AND idReponse = :idReponse');
+                $req->bindValue(':id', $idQuestion, PDO::PARAM_INT);
+                $req->bindValue(':idReponse', $idReponse, PDO::PARAM_INT);
                 $req->execute();
             } catch (Exception $erreur) {
                 die('Erreur : '.$erreur->getMessage());
