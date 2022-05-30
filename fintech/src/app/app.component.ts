15ddd90496cccd0d3d475725adc34729d94c5ece
Zahjen <zahjen.dev@gmail.com>
2022-05-30 23:19:48 +0200

    refresh

--- fintech/src/app/app.component.ts
+++ fintech/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { tab } from 'src/variable/script/nav-bar-data';
 
 @Component({
@@ -6,7 +7,18 @@ import { tab } from 'src/variable/script/nav-bar-data';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fintech';
   tab = tab;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.goToDashboardOnRefresh();
+  }
+
+  // Méthode permettant de retourner au dashboard au refresh de la page car je ne pense pas pouvoir gérer pour le moment le stockage local et envoie à la BdD une fois les divers formulaire fini
+  private goToDashboardOnRefresh() : void {
+    this.router.navigate(['']);
+  }
 }
